48f36051d0f98a77e24ad771c59af67d919df6d7
Santino Mazza <mazzasantino1206@gmail.com>
2022-04-10 20:07:22 -0300

    Create stub macro

--- src/main.c
+++ src/main.c
@@ -3,7 +3,10 @@
 
 #include "msvbvm.h"
 
+#define stub(fmt, ...) printf("STUB:%s: " fmt, __FUNCTION__, ##__VA_ARGS__)
+
 DWORD ThunRTMain(struct vb_header* vb_header) {
+    stub("%p\n", vb_header);
     vb_header->main_function();
     return 0;
 }
